c5a93a46d3ac490a88abc3e58445fd390dcd75a8
= <=>
2023-06-28 09:36:10 +0100

    This is a message!

---
+++ Main/classifiers.py
@@ -0,0 +1,129 @@
+import numpy as np
+from cvxopt import matrix, solvers
+
+import kernel_SVM_functions as kSVM
+import QUBO_SVM_functions as qSVM
+import simulated_annealing as annealing
+
+class SoftMarginKernelClassifier(object):
+
+    def __init__(self, C, kernel_func, param):
+        self.kernel_function = kernel_func
+        self.C = C
+        self.param = param
+        self.support_vectors = None
+        self.support_targets = None
+        self.support_alphas = None
+        self.support_ids = None
+
+    def fit(self, X_train, t_train):
+        #Creates self.model, support vectors and bias for later use.
+        P, q, G, h, A, b = kSVM.kSVM_matrices(X_train, t_train, self.C, self.kernel_function, self.param)
+        self.alphas = kSVM.train_qp(P, q, G, h, A, b)
+        if self.alphas is not None:
+            self.model = kSVM.get_support_vectors(X_train, t_train, self.alphas, self.C)
+            self.support_ids, self.support_vectors, self.support_targets, self.support_alphas = kSVM.get_support_vectors(X_train, t_train, self.alphas, self.C)
+            self.bias = kSVM.discriminant_bias(X_train, t_train, self.alphas, self.C, self.kernel_function, self.param)
+        return self
+    
+    def predict(self, X_test):
+        return np.sign(self.decision_function(X_test))
+
+    def decision_function(self, X_test):
+        return kSVM.score_kSVM(X_test, self.support_vectors, self.support_targets, self.support_alphas, self.kernel_function, self.param, self.bias)
+
+    def predict_proba(self, X_test):
+        scores = self.decision_function(X_test)
+        probs = 1 / (1 + np.exp(-scores))
+        return probs
+    
+
+class QUBOSoftMarginClassifier(object):
+
+    def __init__(self, B, K, R, kernel_func, param):
+        """
+        B: encoding base
+        K: dimension of encoded vector
+        R: regularisation for alpha.T @ t constraint
+        kernel_func: kernel function. e.g. rbf kernel
+        param: parameter for kernel function
+        """
+        self.B = B
+        self.K = K
+        self.R = R
+        self.C = np.sum([B ** i for i in range(K)])
+        self.kernel_func = kernel_func
+        self.param = param
+
+        self.cooling_param = 5
+        self.T0 = 10
+        self.annealing_iterations = 100
+        self.m_rate = 1 / K
+
+        self.ObjectiveFunction = None
+
+        self.alphas = None
+        self.QUBO_minimisation_score = None
+
+        self.support_ids = None
+        self.support_vectors = None
+        self.support_targets = None
+        self.support_alphas = None
+
+        self.bias = None
+
+    def make_QUBO_problem(self, X_train, t_train):
+        """Given X_train, t_train data to train the model. 
+        Creates Q, q matrices and defines the objective function for the QUBO problem.
+        """
+        self.Q, self.q = qSVM.make_QUBO_matrices(X_train, t_train, self.kernel_func, self.param, self.B, self.K, self.R)
+        self.ObjectiveFunction = annealing.QObjectiveFunction(self.Q, self.q)    
+        return self
+    
+    def set_annealing_parameters(self, cooling_param, m_rate, annealing_iterations, T0):
+        """
+        annealing params are set as defualt, but they can be changed with this method. 
+        """
+        self.cooling_param = cooling_param
+        self.m_rate = m_rate
+        self.annealing_iterations = annealing_iterations
+        self.T0 = T0
+        return self
+    
+    def set_bias(self, bias):
+        """Sets the model's bias paramter --- used for the decision function --- to the given input"""
+        self.bias = bias
+        return self
+    
+    def fit(self, X_train, t_train):
+        """
+        X_train, t_train: The same data which was used to create the QUBO matrices
+        """
+        assert self.ObjectiveFunction, "Objective function not set. Run make_QUBO_problem method"
+
+        encoded_alphas = annealing.run_annealing(self.cooling_param, self.m_rate, self.annealing_iterations, self.T0, self.ObjectiveFunction)
+        self.ObjectiveScore = self.ObjectiveFunction.evaluate(encoded_alphas)
+        self.alphas = qSVM.decode(encoded_alphas, self.B, self.K)
+
+        self.support_ids, self.support_vectors, self.support_targets, self.support_alphas = kSVM.get_support_vectors(X_train, t_train, self.alphas, self.C)
+        self.bias = kSVM.discriminant_bias(X_train, t_train, self.alphas, self.C, self.kernel_func, self.param)
+
+        return self
+    
+    def decision_function(self, X_test):
+        return kSVM.score_kSVM(X_test, self.support_vectors, self.support_targets, self.support_alphas, self.kernel_func, self.param, self.bias)
+
+    def predict_proba(self, X_test):
+        scores = self.decision_function(X_test)
+        probs = 1 / (1 + np.exp(-scores))
+        return probs
+    
+    def predict(self, X_test):
+        """Returns the predicted class for the test data"""
+        return np.sign(self.decision_function(X_test))
+    
+def main():
+    print("This file contains SoftMarginKernelClassifier and QUBOSoftMarginClassifier classes. \n")
+
+if __name__ == "__main__":
+    main()
---
+++ Main/kernel_SVM_functions.py
@@ -0,0 +1,104 @@
+import numpy as np
+from cvxopt import matrix, solvers
+
+def rbf_kernel(x, y, param):
+    """
+    calculates the gaussian kernel transformation between two vectors x and y. 
+    x, y: vectors should have the same shape (d, ) where d is the dimension of the problem.
+    param: a 
+    """
+    diff = x - y
+    K = np.exp(-param * (diff.T @ diff))
+    return K
+
+def train_qp(P ,q, G, h, A, b):
+    """
+    returns the solution from cvxopt qp solver
+    """
+    solvers.options['show_progress'] = False
+    try:
+        sol = solvers.qp(P ,q, G, h, A, b)
+    except ValueError:
+        #Handling if cvxopt cannot solve the problem.
+        return None
+    
+    return np.array(sol['x'])
+
+def kSVM_matrices(X, t, C, kernel_func, gamma):
+    """
+    returns kernel SVM matrices P, q, G, h, A, b, for use in cvxopt solvers.qp() function. 
+    """
+    N, d = X.shape
+    assert t.shape == (N, 1), f"t is required to be shape ({X.shape[0]}, 1), but is shape {t.shape}"
+
+    P = matrix(np.array([[kernel_func(X[i], X[j], gamma) * t[i][0] * t[j][0] for i in range(N)] for j in range(N)]), tc='d')
+    q = matrix(np.full(N, -1), tc='d')
+    G = matrix(np.vstack((np.diag(-1 * np.ones(N)), np.diag(np.ones(N)))), tc='d')
+    h = matrix(np.vstack((np.zeros((N, 1)), C * np.ones((N, 1)))), tc='d')
+    A = matrix(t.reshape(1, -1), tc='d')
+    b = matrix(np.array([0]), tc='d')
+
+    return P, q, G, h, A, b
+
+def discriminant_bias(X_train, t_train, alphas, C, kernel_func = None, gamma = None):
+    """
+    to calculate the 'bias' for the discriminant, linear or non-linear. 
+    X_train: data matrix shape (n, d) (d dimensional problem)
+    t_train: array of targets. shape (n, 1)
+    alphas: result of lagrangian dual problem. shape (n, 1)
+    C: regularisation parameter used. Real number.
+    kernel_func: function used to calculate the kernel transformation for two vectors (x, y). If not given, we assume that 
+    no kernel is used. This is the same as the dot product between the two vectors x.T * x.
+    """
+
+    N, d = X_train.shape
+    assert t_train.shape == (N, 1), f"t has shape {t_train.shape}, but should have shape ({N}, 1)"
+    assert alphas.shape == (N, 1), f"alphas has shape {alphas.shape}, but should have shape ({N}, 1)"
+
+    denominator = np.dot(alphas.reshape(1, -1), (C - alphas))
+
+    if kernel_func:
+        H = np.array([[kernel_func(X_train[m], X_train[n], gamma) for m in range(N)] for n in range(N)])
+    else:
+        H = X_train @ X_train.T
+
+    v = t_train - H @ (alphas * t_train)
+    numerator = np.dot(alphas.reshape(1, -1), (C - alphas) * v)
+
+    return numerator[0][0] / denominator[0][0]
+
+def get_support_vectors(X_train, t_train, alphas, C):
+    #Returns support ids, support vectors, support targets, and support alphas for use in calculating score.
+    N = X_train.shape[0]
+    assert (t_train.shape[0] == N) and (alphas.shape[0] == N), "either t_train or alphas does not match the shape of X_train"
+    #Find support vectors and their alphas and corresponding targets
+    support_ids = np.where(np.logical_and(alphas > 1e-4, alphas < C - 1e-4).flatten())[0]
+    #if len(support_ids) == 0:
+    #    print('alphas: ', alphas)
+    #    raise Exception("No support vectors found")
+
+    support_vectors = X_train[support_ids]
+    support_targets = t_train[support_ids]
+    support_alphas = alphas[support_ids]
+
+    return support_ids, support_vectors, support_targets, support_alphas
+
+def score_kSVM(X_test, support_vectors, support_targets, support_alphas, kernel_func, param, bias):
+    """alphas: list of the alpha values corresponding to each vector in X_train
+    t_train: targets of the vectors in X_train
+    C: maximum value an alpha can be corresponding to sum(B**i for i in range(K)) in the encoding
+    kernel_func and param: to calculate the kernel function of two vectors
+    bias: calculated using discriminant_bias() in SVM_functions.py
+    """
+    H = np.array([[support_alphas[n][0] * support_targets[n][0] * kernel_func(support_vectors[n], X_test[m], param) for n in range(len(support_vectors))] for m in range(len(X_test))])
+    y = np.sum(H, axis = 1) + bias
+    return y
+
+def main():
+    print("This file contians functions to assist with kernel soft margin classifiers. \n")
+    print("""The following funcitons are available: \n 
+    - score_kSVM \n - get_support_vectors \n - discriminant_bias \n - kSVM_matrices \n - train_qp \n - rbf_kernel \n""")
+    pass
+
+if __name__ == '__main__':
+    main()
