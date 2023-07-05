c6a75a9a365f0a193684adbfe65eedd6302244e2
Isaac Copson <isaaccopson@hotmail.com>
2023-07-05 12:36:18 +0000

    initial changes

--- Main/quantum-annealing.py
+++ Main/quantum-annealing.py
@@ -1,6 +1,10 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-from dimond import BinaryQuadraticModel
+from classifiers import QUBOSoftMarginClassifier
+from kernel_SVM_functions import rbf_kernel
+
+from dimod import BinaryQuadraticModel
 from dwave.system.samplers import DWaveSampler
 
 def quantum_fit(X_train, t_train, model):
@@ -18,6 +22,26 @@ def main():
     #Get data from file
     #create QUBO SVM classifier
     #Train using quantum fitness function which uses bqm
+    filepath = 'synth_data/synth_0.3.csv'
+
+    data = np.loadtxt(filepath, delimiter = ',')
+    X = data[:, :-1]
+    t = data[:, -1]
+
+    X_train, X_test, t_train, t_test = train_test_split(X, t, train_size = 0.4, shuffle = True, stratify = t)
+    t_train = t_train.reshape(-1, 1)
+    t_test = t_test.reshape(-1, 1)
+
+    B = 2
+    K = 3
+    R = 1
+    kernel_func = rbf_kernel
+    gamma = 4
+
+    QSVM = QUBOSoftMarginClassifier(B, K, R, kernel_func, gamma)
+    QSVM = QSVM.make_QUBO_problem(X_train, t_train)
+
+    print(QSVM.Q.shape, QSVM.q.shape)
 
     pass
 
