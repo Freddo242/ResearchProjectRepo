18e887e9b044cfc9ff7c1cdfa28dca1117cf0b20
Isaac Copson <isaaccopson@hotmail.com>
2023-07-05 14:54:12 +0000

    Not working yet

--- Main/quantum-annealing.py
+++ Main/quantum-annealing.py
@@ -2,19 +2,35 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 from classifiers import QUBOSoftMarginClassifier
-from kernel_SVM_functions import rbf_kernel
+from kernel_SVM_functions import rbf_kernel, discriminant_bias
+from QUBO_SVM_functions import decode
 
 from dimod import BinaryQuadraticModel
+from dwave.system.composites import EmbeddingComposite
 from dwave.system.samplers import DWaveSampler
 
 def quantum_fit(X_train, t_train, model):
     """
-    Uses DWave's 
+    Uses DWave's sampler and binary quadratic model to solve our qubo.
     """
 
     assert model.ObjectiveFunction, "Objective function not set. Run make_QUBO_problem method"
 
+    #create the BinaryQuadraticModel
 
+    q = model.q 
+    linear = {i: q[i][0] for i in range(q.shape[0])}
+
+    Q = model.Q
+    quadratic = {(i, j): Q[i, j] for i in range(Q.shape[0]) for j in range(Q.shape[1])}
+
+    bqm = BinaryQuadraticModel(linear, quadratic, 0, "BINARY")
+    #Set up sampler
+    sampler = EmbeddingComposite(DWaveSampler())
+    #Sample the results
+    sample_set = sampler.sample(bqm)
+
+    return sample_set
 
 
 def main():
@@ -41,8 +57,12 @@ def main():
     QSVM = QUBOSoftMarginClassifier(B, K, R, kernel_func, gamma)
     QSVM = QSVM.make_QUBO_problem(X_train, t_train)
 
-    print(QSVM.Q.shape, QSVM.q.shape)
-
+    samples = quantum_fit(X_train, t_train, QSVM)
+    print(samples, '\n')
+    print(type(samples), '\n')
+    with open('sample_dump', 'w') as f:
+        f.write(samples)
+    
     pass
 
 
