13e87bf558e182fc26e012aec7db411135dafc22
= <=>
2023-07-05 12:37:37 +0100

    beginning

---
+++ Main/quantum-annealing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from dimond import BinaryQuadraticModel
+from dwave.system.samplers import DWaveSampler
+
+def quantum_fit(X_train, t_train, model):
+    """
+    Uses DWave's 
+    """
+
+    assert model.ObjectiveFunction, "Objective function not set. Run make_QUBO_problem method"
+
+
+
+
+def main():
+
+    #Get data from file
+    #create QUBO SVM classifier
+    #Train using quantum fitness function which uses bqm
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
