f9986ade3e556221342f4416b263ff0057230ed4
Isaac Copson <isaaccopson@hotmail.com>
2023-07-20 17:54:59 +0000

    ready to run next parameters

--- Main/quantum_annealing/quantum_annealing_functions.py
+++ Main/quantum_annealing/quantum_annealing_functions.py
@@ -5,10 +5,19 @@ sys.path.append('../')
 import numpy as np 
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
+from time import time
 
 from quantum_classifier import QSVMq
 from metric_functions import compute_auc_from_scores, compute_accuracy
 
+
+def include_indices(param_combinations, to_skip):
+    #Takes in a list of combinations to skip and returns a boolean array of those combinations to include
+    indices = []
+    for arr in to_skip:
+        indices.append(np.array([(param_combinations[i, :, 1] == arr).all() for i in range(param_combinations.shape[0])], dtype=int))
+
+    return ~np.sum(indices, axis = 0).astype(bool)
 
 def stratified_sample(t, size = 20):
     """picks indexes for values in arr such that same number of values for each class appear in the sample"""
@@ -44,55 +53,78 @@ def QA_calibration(X_train, t_train, B_values, K_values, R_values, gamma_values,
     auroc = np.zeros((len(B_values), len(K_values), len(R_values), len(gamma_values)))
     accuracy = np.zeros((len(B_values), len(K_values), len(R_values), len(gamma_values)))
 
-    for i, B in enumerate(B_values):
-        for j, K in enumerate(K_values):
-            for k, R in enumerate(R_values):
-                for l, gamma in enumerate(gamma_values):
-                    print(B, K, R, gamma, '\n')
-                    #Make new directory to store results for this hyper-parameter combination.
-                    os.mkdir(f'../QA_results/{B, K, R, gamma}')
+    param_combinations = np.array([[[i, B], [j, K], [k, R], [l, gamma]] for i, B in enumerate(B_values) 
+                                                            for j, K in enumerate(K_values) 
+                                                            for k, R in enumerate(R_values)
+                                                            for l, gamma in enumerate(gamma_values)])
 
-                    #Setting up the classifier for cross validation
-                    qsvmq = QSVMq(B, K, R, kernel_func, gamma)
+    #This is an array of the parameter combinations we have already completed. The code is too long to do all at once.
+    params_to_skip = np.array([[2, 3, 1, 2]])
+    #index of the combinations to include
+    include_index = include_indices(param_combinations, params_to_skip)
+
+    param_combinations = param_combinations[include_index, :, :]
+
+    print(param_combinations[:, :, 1])
+
+    for (i, B), (j, K), (k, R), (l, gamma) in param_combinations:
+
+        print(B, K, R, gamma, '\n')
+
+        t0 = time()
+
+        #Make new directory to store results for this hyper-parameter combination.
+        os.mkdir(f'../QA_results/{B, K, R, gamma}')
+
+        #Setting up the classifier for cross validation
+        qsvmq = QSVMq(B, K, R, kernel_func, gamma)
                     
-                    set_auc = []
-                    set_acc = []
+        set_auc = []
+        set_acc = []
                     
-                    for s in range(10):
-                        #Making subdirectory for the sample
-                        os.mkdir(f'../QA_results/{B, K, R, gamma}/sample-{s + 1}/')
+        for s in range(10):
+
+            t1 = time()
+            #Making subdirectory for the sample
+            os.mkdir(f'../QA_results/{B, K, R, gamma}/sample-{s + 1}/')
                         
-                        #stratified sample based on t of size 20
-                        sample_index = stratified_sample(t_train, 20)
+            #stratified sample based on t of size 20
+            sample_index = stratified_sample(t_train, 20)
 
-                        X_train_sample = X_train[sample_index]
-                        t_train_sample = t_train[sample_index]
+            X_train_sample = X_train[sample_index]
+            t_train_sample = t_train[sample_index]
 
-                        #filepath to save the fold models from the QA
-                        filepath = f'../QA_results/{B, K, R, gamma}/sample-{s + 1}/'
+            #filepath to save the fold models from the QA
+            filepath = f'../QA_results/{B, K, R, gamma}/sample-{s + 1}/'
 
-                        #Cross validating training and test data for this sample giving us an accurate auroc and accuracy
-                        auroc_results, accuracy_results = QA_cross_validate(X_train_sample, t_train_sample, qsvmq, filepath)
+            #Cross validating training and test data for this sample giving us an accurate auroc and accuracy
+            auroc_results, accuracy_results = QA_cross_validate(X_train_sample, t_train_sample, qsvmq, filepath)
 
-                        #Taking the mean of the metrics across each of the folds
-                        set_auc.append(np.mean(auroc_results))
-                        set_acc.append(np.mean(accuracy_results))
+            #Taking the mean of the metrics across each of the folds
+            set_auc.append(np.mean(auroc_results))
+            set_acc.append(np.mean(accuracy_results))
 
-                    #final metrics are the average over each sample
-                    auroc[i, j, k, l] = np.mean(set_auc)
-                    accuracy[i, j, k, l] = np.mean(set_acc)
+            t2 = time()
+            print('Sample time', t2 - t1)
 
-                    wait = global_wait
+        #final metrics are the average over each sample
+        auroc[i, j, k, l] = np.mean(set_auc)
+        accuracy[i, j, k, l] = np.mean(set_acc)
 
-                    while wait:
-                        x = input('waiting...')
+        np.save('../QA_results/QA_auroc', auroc)
+        np.save('../QA_results/QA_accuracy', accuracy)
 
-                        if x == 'next':
-                            wait = False
+        wait = global_wait
 
-                        if x == 'goforit':
-                            global_wait = False
-                            break
+        while wait:
+            x = input('waiting...')
+
+            if x == 'next':
+                wait = False
+
+            if x == 'goforit':
+                global_wait = False
+                break
 
     np.save('../QA_results/QA_auroc', auroc)
     np.save('../QA_results/QA_accuracy', accuracy)
